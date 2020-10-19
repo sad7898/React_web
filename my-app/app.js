f05292661a2a07cdaf9a076e3977398d8a9c9ef7
sad7898 <siradhee_k@hotmail.com>
2020-10-19 18:10:19 +0700

    'testing'

--- my-app/app.js
+++ my-app/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
 const uuid = require('uuid');
+const hello = 'hello'
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, 'build')));
 const mongoose = require('mongoose')
